d90bc72689762f382d106a684b4d8b1e359a1dfe
KazuneHosaka <kazune_hosaka@kinjiro-e.com>
2022-12-13 16:55:29 +0900

    機能名の数字を半角に修正

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,15 +13,15 @@ public class Main {
     
     public static void main(String args[]){
         
-        System.out.println("機能１");
+        System.out.println("機能1");
         
-        System.out.println("機能２");
+        System.out.println("機能2");
 
-        System.out.println("機能３");
+        System.out.println("機能3");
         
-        System.out.println("機能４");
+        System.out.println("機能4");
         
-        System.out.println("機能５");
+        System.out.println("機能5");
     }
     
 }
